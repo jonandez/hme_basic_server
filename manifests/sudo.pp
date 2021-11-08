a90e54d4947059851293f92dfb4f180e33b2a14c
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-08 17:59:01 -0300

    Add code

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -6,7 +6,8 @@
 #   include hme_masterless_basic_server::sudo
 class hme_masterless_basic_server::sudo {
 
-  require hme_masterless_basic_server::requirements
+  include hme_masterless_basic_server::requirements
+  include hme_masterless_basic_server::user
   include sudo
   # require ::requirements
   Class['hme_masterless_basic_server::sudo'] ~> Class['hme_masterless_basic_server::requirements']
