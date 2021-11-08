5fd179469c3d9fcb28f149b4d6ee25436951b0ef
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-08 18:12:50 -0300

    Add code

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,9 @@
 # @example
 #   include hme_masterless_basic_server
 class hme_masterless_basic_server  {
-  include hme_masterless_basic_server::requirements
+  # include hme_masterless_basic_server::requirements
   # include hme_masterless_basic_server::cron
   # include hme_masterless_basic_server::user
-  # include hme_masterless_basic_server::sudo
+  include hme_masterless_basic_server::sudo
 }
 
