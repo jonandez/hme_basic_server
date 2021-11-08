1c5cb3df53cb1ea51b81deea0a6bd3f433358e11
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-08 14:25:31 -0300

    Add code

--- manifests/requirements.pp
+++ manifests/requirements.pp
@@ -5,13 +5,11 @@
 # @example
 #   include hme_masterless_basic_server::requirements
 class hme_masterless_basic_server::requirements {
-  # $modules = ['saz-sudo --version 7.0.2', 'puppet-firewalld --version 4.4.0']
+  $modules = ['saz-sudo --version 7.0.2', 'puppet-firewalld --version 4.4.0']
 
-  # exec { "puppet module install ${modules}":
-  #   command => 
-  #   path    => '/opt/puppetlabs/puppet/bin/',
-  # }
-  module { 'saz/sudo ':
-    ensure => '7.0.2',
+  $modules.each |String $module| {
+    exec { "puppet module install ${module}":
+      path    => '/opt/puppetlabs/puppet/bin/',
+    }
   }
 }
