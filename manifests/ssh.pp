06b607b5d94bdb5ded52ba00e56e61f2bec66d1a
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-08 12:38:00 -0300

    Add base module code

---
+++ manifests/ssh.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include hme_masterless_basic_server::ssh
+class hme_masterless_basic_server::ssh {
+}
