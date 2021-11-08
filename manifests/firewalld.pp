70ae2882f3e97d388f1ac91e5ea297e11e3d3a6d
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-08 19:15:45 -0300

    Add code

--- manifests/firewalld.pp
+++ manifests/firewalld.pp
@@ -6,32 +6,33 @@
 #   include hme_masterless_basic_server::firewalld
 class hme_masterless_basic_server::firewalld {
 
+  include firewalld
 # Drop all traffic and allow pors 8080, 443, 22
 
-    firewalld_port { 'Open port 8080 in the public zone':
+  firewalld_port { 'Open port 8080 in the public zone':
     ensure   => present,
     zone     => 'public',
     port     => 8080,
     protocol => 'tcp',
   }
 
-    firewalld_port { 'Open port 443 in the public zone':
+  firewalld_port { 'Open port 443 in the public zone':
     ensure   => present,
     zone     => 'public',
     port     => 443,
     protocol => 'tcp',
   }
 
-    firewalld_port { 'Open port 22 in the public zone':
+  firewalld_port { 'Open port 22 in the public zone':
     ensure   => present,
     zone     => 'public',
     port     => 22,
     protocol => 'tcp',
   }
 
-    firewall { '999 drop all':
-      proto  => 'all',
-      action => 'drop',
-      before => undef,
+  firewall { '999 drop all':
+    proto  => 'all',
+    action => 'drop',
+    before => undef,
   }
 }
