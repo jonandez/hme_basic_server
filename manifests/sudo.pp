a2838fe3f88ddb5e6e52d2a7fca0c143f1f9ff43
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-08 18:16:07 -0300

    Add code

--- manifests/sudo.pp
+++ manifests/sudo.pp
@@ -6,6 +6,8 @@
 #   include hme_masterless_basic_server::sudo
 class hme_masterless_basic_server::sudo {
 
+  require hme_masterless_basic_server::requirements
+
   sudo::conf { 'hme':
     priority => 60,
     content  => 'hme ALL=(ALL) NOPASSWD: ALL',
