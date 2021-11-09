16dcc1dba36bc0c5f8570d8e1817fbf0977ee563
Jose Hernandez <globant-jhernandez@hme.com>
2021-11-09 09:35:09 -0300

    Add code

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -6,8 +6,8 @@
 #   include hme_masterless_basic_server::cron
 class hme_masterless_basic_server::cron {
 
-  # file { '/etc/puppetlabs/puppet/cron.sh':
-  file { 'cron.sh':
+  file { '/etc/puppetlabs/puppet/cron.sh':
+  # file { 'cron.sh':
     source => 'puppet:///modules/hme_masterless_basic_server/cron.sh',
     owner  => 'root',
     type   => file,
